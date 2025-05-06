89d9f783c7ddf6138b87472ea368e2a70b848eaf
maicarem <60804140+maicarem@users.noreply.github.com>
2025-05-06 21:07:40 +0000

    Deploying to gh-pages from @ maicarem/maicarem.github.io@125a0998e3fa867eebb08c68aad92dc887e8af69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please download my CV for more information",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -435,11 +435,16 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-mendeleyev-39-s-dream",
+          title: 'Mendeleyev&amp;#39;s Dream',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/mendeylev/";
+            },},{id: "books-voices-from-chernobyl-the-oral-history-of-a-nuclear-disaster",
+          title: 'Voices from Chernobyl The Oral History of a Nuclear Disaster',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/voices_from_Chernobyl/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -501,14 +506,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%72%75%6F%6E%67.%61%6E%68@%75%66%6C.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/maianhtruong1509", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +527,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=GR-3OwkAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/maianhtruong1509/", "_blank");
         },
       },{
       id: 'light-theme',
