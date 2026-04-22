7732428d95fac5a5db600947cc77e411691ffb0f
maicarem <60804140+maicarem@users.noreply.github.com>
2026-04-22 15:24:24 +0000

    Deploying to gh-pages from @ maicarem/maicarem.github.io@b349613242ae7fcd6b527732bbc8065d77a26673 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,24 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{
+  },{id: "books-mendeleyev-39-s-dream",
+          title: 'Mendeleyev&amp;#39;s Dream',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/mendeylev/";
+            },},{id: "books-voices-from-chernobyl-the-oral-history-of-a-nuclear-disaster",
+          title: 'Voices from Chernobyl The Oral History of a Nuclear Disaster',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/voices-from-chernobyl/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%74%72%75%6F%6E%67.%61%6E%68@%75%66%6C.%65%64%75", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
